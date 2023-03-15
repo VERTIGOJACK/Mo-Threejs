3fe3dd74d78859278e69d730f88ae7e10d61cbe8
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-03-15 15:33:57 +0100

    webpack threejs starter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import panzoom from "panzoom";
-import Flicking from "@egjs/flicking";
-import "@egjs/flicking/dist/flicking.css";
-
-const zoomelement = document.getElementById("panzoom");
-panzoom(zoomelement);
-
-const myflicker = document.getElementById("my-flicking");
-const flick = new Flicking(myflicker, { circular: true });
-
-console.log(flick);
-console.log(flick);
+import * as THREE from "three";
